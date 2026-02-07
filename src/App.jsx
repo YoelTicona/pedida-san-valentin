5c7a34eb1edf678054fa135ae325ade8f622db41
YoelTicona <yoelticonalaura@gmail.com>
2026-02-07 11:25:17 -0400

    ultimos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
 
   return (
     <>
+    <head>
+    <title>Tengo una pregunta para ti... 💌</title>
+    <meta property="og:title" content="Para Liz ✨" />
+    <meta property="og:description" content="¿Me concederías este honor?" />
+    <meta property="og:image" content="https://media.tenor.com/2Y29k2dJ5MAAAAAi/chibird-chibird-art.gif" />
+    </head>
       <ValentinePage />
     </>
   )
